01985528928c5fba35817d76ed7eb71f92bafd83
DESKTOP-5VLNGRK\angle <angiel.4work@gmail.com>
2023-10-26 20:19:08 -0400

    camera controller code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,7 +7,7 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     public float offset;
     public float offsetSmoothing;
-    private Vector2 playerPosition;
+    private Vector3 playerPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +18,14 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerPosition = new Vector2(player.transform.position.x, player.transform.position.y);
+        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
         if (player.transform.localScale.x > 0f)
         {
-            playerPosition = new Vector2(playerPosition.x + offset, playerPosition.y);
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
         }
         else
         {
-            playerPosition = new Vector2(playerPosition.x - offset, playerPosition.y);
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
         }
         transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime); 
 
